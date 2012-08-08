d533d4981d0c62966c2f4c447942bb319d1cad25
David Zeng <dzeng@pc.local>
2012-08-07 17:34:43 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covoi::Application.config.secret_token = '5b754395328767f69118c95d3bb46749b5f9e20dcfd94e8278656c1245242e2f8c8b093d0b259f73d49c35bda47516103a93ea6bee704e44f64ff8e9a9d7d8dc'
