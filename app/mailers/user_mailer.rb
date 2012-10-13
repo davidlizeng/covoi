431d3c79d753541b898d319528eadb9b43acf9aa
David Zeng <dzeng@rescomp-12-240682.stanford.edu>
2012-10-13 03:18:41 -0700

    Fixed another bug with env config vars

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ActionMailer::Base
 
   default :from => "noreply@ridegrouped.com"
-  default_url_options[:host] = SERVER_HOSTNAME
 
   def registration_confirmation(user)
     @user = user
